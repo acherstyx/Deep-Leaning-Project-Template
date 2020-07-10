2122c0f1ad556cd2267936540712d321b1c21741
AcherStyx <SYJ.Micro@outlook.com>
2020-07-10 14:27:00 +0800

    Add config template

---
+++ config_template.py
@@ -0,0 +1,40 @@
+import json
+
+
+class ConfigTemplate:
+    """>> Config Templates <<
+
+    Use __init__ to set the value of your config.
+
+    e.g.
+
+    class MyConfig(ConfigTemplate):
+        def __init__(self, learning_rate):
+            self.LEARNING_RATE = learning_rate
+    """
+
+    def dump(self, json_file: str = None) -> object:
+        """
+        Dump the config to json string. Use this to keep your config.
+
+        :param json_file:  if json_file is specified, the result (json string) will be save to that file
+        :return:
+        """
+        if json_file is not None:
+            with open(json_file, "w") as f:
+                json.dump(self.__dict__, f)
+        return json.dumps(self.__dict__)
+
+    @staticmethod
+    def load(json_file: str) -> object:
+        """
+        Load config from a json file.
+
+        :param json_file:
+        :return: object with same class
+        """
+        print(__class__)
+        with open(json_file, "r") as f:
+            new_config = __class__()
+            new_config.__dict__ = json.load(f)
+            return new_config
